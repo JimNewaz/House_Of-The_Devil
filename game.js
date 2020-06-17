cad4034208ceacbaa688983ed9ab0383ebca7bc7
JimNewaz <miznewaz@gmail.com>
2020-06-17 14:25:13 +0600

    Initial Commit

---
+++ game.js
@@ -0,0 +1,86 @@
+function theyfire1(){
+    life=life-1;
+    
+         HIT6.load();
+          HIT6.play();
+    var get1 = document.querySelector("#they1");
+     get1.src="images/skel11.png";
+ 
+    setTimeout(removd,150);
+    
+    
+    
+    if (life<=0){
+        setTimeout(opps,100); 
+    }
+    
+    else {};
+    scores();
+    
+    
+}
+
+ function theyfire2(){
+    life=life-1;
+    
+         HIT6.load();
+          HIT6.play();
+    var get2 = document.querySelector("#they2");
+     get2.src="images/skel11.png";
+ 
+    setTimeout(removd2,150);
+    
+    
+    
+    if (life<=0){
+        setTimeout(opps,100); 
+    }
+    
+    else {};
+     scores();
+    
+    
+}
+
+ function theyfire3(){
+    life=life-1;
+    
+         HIT6.load();
+          HIT6.play();
+    var get3 = document.querySelector("#they3");
+     get3.src="images/skel11.png";
+ 
+    setTimeout(removd3,150);
+    
+    
+    
+    if (life<=0){
+        setTimeout(opps,100); 
+    }
+    
+    else {};
+ scores();   
+ 
+}
+
+ function theyfire4(){
+    life=life-1;
+    
+         HIT6.load();
+          HIT6.play();
+    var get4 = document.querySelector("#they4");
+     get4.src="images/skel11.png";
+ 
+    setTimeout(removd4,150);
+    
+    
+    
+    if (life<=0){
+       setTimeout(opps,100); 
+    }
+    
+    else {};
+    scores();
+    
+}
+
